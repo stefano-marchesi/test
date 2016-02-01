eb497c0ce7d3f94ce80bdef48e014520236c0b0b
stefano-marchesi <stefano.marchesi1984@gmail.com>
2016-02-01 16:38:05 +0100

    autocommit

---
+++ upgradeBuild.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/expect -f
+spawn ssh root@5.189.145.126
+expect "*assword: "
+send "qoK8VLMl8CJbW\r"
+expect "*#"
+send "cd /home/build \r"
+expect "*#"
+send "git pull https://github.com/stefano-marchesi/test.git \r"
